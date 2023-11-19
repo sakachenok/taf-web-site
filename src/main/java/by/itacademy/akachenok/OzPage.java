a8eef393d9e420f2633ff6041f8502540ab687d8
sakachenok <akachenoksveta@gmail.com>
2023-11-19 16:51:51 +0300

    The tests were optimized. New methods were added into OzPage.java

--- src/main/java/by/itacademy/akachenok/OzPage.java
+++ src/main/java/by/itacademy/akachenok/OzPage.java
@@ -1,11 +1,54 @@
 package by.itacademy.akachenok;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class OzPage {
-    public String linkEnterLocator = "//*[@id=\"top-page\"]/header/div[1]/div/div[1]/a";
-    public String linkEmailLoginFormLocator = "//*[@id=\"loginFormLoginEmailLink\"]";
-    public String inputEmailLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[1]/input";
-    public String inputPasswordLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[2]/input";
-    public String buttonEnterLocator = "//*[@id=\"loginForm\"]/button";
-    public String errorMessageEmailDoesNotExistLocator = "//*[@id=\"test\"]/div/div";
-    public String textEnterLoginFormLocator = "//*[@id=\"loginPopupIntro\"]";
+    private String linkEnterLocator = "//*[@id=\"top-page\"]/header/div[1]/div/div[1]/a";
+    private String linkEmailLoginFormLocator = "//*[@id=\"loginFormLoginEmailLink\"]";
+    private String inputEmailLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[1]/input";
+    private String inputPasswordLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[2]/input";
+    private String buttonEnterLocator = "//*[@id=\"loginForm\"]/button";
+    private String errorMessageEmailDoesNotExistLocator = "//*[@id=\"test\"]/div/div";
+    private String textEnterLoginFormLocator = "//*[@id=\"loginPopupIntro\"]";
+    WebDriver driver;
+    public OzPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void clickLinkEnter(){
+        WebElement linkEnter = driver.findElement(By.xpath(linkEnterLocator));
+        linkEnter.click();
+    }
+
+    public void clickLinkEmailLoginForm(){
+        WebElement linkEmailLoginForm = driver.findElement(By.xpath(linkEmailLoginFormLocator));
+        linkEmailLoginForm.click();
+    }
+
+    public void sendKeysInputEmail(String newEmail){
+        WebElement inputEmail = driver.findElement(By.xpath(inputEmailLocator));
+        inputEmail.sendKeys(newEmail);
+    }
+
+    public void sendKeysInputPassword(String newPassword){
+        WebElement inputPassword = driver.findElement(By.xpath(inputPasswordLocator));
+        inputPassword.sendKeys(newPassword);
+    }
+
+    public void clickButtonEnter(){
+        WebElement buttonEnter = driver.findElement(By.xpath(buttonEnterLocator));
+        buttonEnter.click();
+    }
+
+    public String getTextErrorMessageEmailDoesNotExist (){
+        WebElement errorMessageEmailDoesNotExist = driver.findElement(By.xpath(errorMessageEmailDoesNotExistLocator));
+        return errorMessageEmailDoesNotExist.getText();
+    }
+
+    public String getTextEnterLoginForm() {
+        WebElement textEnterLoginForm = driver.findElement(By.xpath(textEnterLoginFormLocator));
+        return textEnterLoginForm.getText();
+    }
 }
--- src/test/java/by/itacademy/akachenok/OzTest.java
+++ src/test/java/by/itacademy/akachenok/OzTest.java
@@ -2,9 +2,7 @@ package by.itacademy.akachenok;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OzTest {
@@ -15,27 +13,14 @@ public class OzTest {
         driver.manage().window().maximize();
         driver.get("https://oz.by/");
 
-        OzPage ozpage = new OzPage();
-
-        WebElement linkEnter = driver.findElement(By.xpath(ozpage.linkEnterLocator));
-        linkEnter.click();
-
-        WebElement linkEmailLoginForm = driver.findElement(By.xpath(ozpage.linkEmailLoginFormLocator));
-        linkEmailLoginForm.click();
-
-        WebElement inputEmail = driver.findElement(By.xpath(ozpage.inputEmailLocator));
-        inputEmail.sendKeys("sveta@test.com");
-
-        WebElement inputPassword = driver.findElement(By.xpath(ozpage.inputPasswordLocator));
-        inputPassword.sendKeys("12346");
-
-        WebElement buttonEnter = driver.findElement(By.xpath(ozpage.buttonEnterLocator));
-        buttonEnter.click();
+        OzPage ozpage = new OzPage(driver);
+        ozpage.clickLinkEnter();
+        ozpage.clickLinkEmailLoginForm();
+        ozpage.sendKeysInputEmail("sveta@test.com");
+        ozpage.sendKeysInputPassword("123456");
+        ozpage.clickButtonEnter();
         Thread.sleep(2000);
-
-        WebElement errorMessageEmailDoesNotExist = driver.findElement(By.xpath(ozpage.errorMessageEmailDoesNotExistLocator));
-        Assertions.assertEquals("Адрес электронной почты не зарегистрирован. Зарегистрироваться", errorMessageEmailDoesNotExist.getText());
-
+        Assertions.assertEquals("Адрес электронной почты не зарегистрирован. Зарегистрироваться", ozpage.getTextErrorMessageEmailDoesNotExist());
         Thread.sleep(2000);
         driver.quit();
     }
@@ -45,15 +30,9 @@ public class OzTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://oz.by/");
-
-        OzPage ozPage = new OzPage();
-
-        WebElement linkEnter = driver.findElement(By.xpath(ozPage.linkEnterLocator));
-        linkEnter.click();
-
-        WebElement textEnterLoginForm = driver.findElement(By.xpath(ozPage.textEnterLoginFormLocator));
-        Assertions.assertEquals("Вход", textEnterLoginForm.getText());
-
+        OzPage ozpage = new OzPage(driver);
+        ozpage.clickLinkEnter();
+        Assertions.assertEquals("Вход", ozpage.getTextEnterLoginForm());
         Thread.sleep(2000);
         driver.quit();
     }
