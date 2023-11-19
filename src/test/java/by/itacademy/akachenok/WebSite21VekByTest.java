e692b125ce88cc0dcb648c4ed524c5478b140e1a
sakachenok <akachenoksveta@gmail.com>
2023-11-19 18:36:56 +0300

    The tests were modified. Methods were added into WebSite21VekByPage.java

--- src/test/java/by/itacademy/akachenok/WebSite21VekByTest.java
+++ src/test/java/by/itacademy/akachenok/WebSite21VekByTest.java
@@ -2,9 +2,7 @@ package by.itacademy.akachenok;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebSite21VekByTest {
@@ -14,27 +12,14 @@ public class WebSite21VekByTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.21vek.by/");
-
-        WebSite21VekByPage webSite21VekByPage = new WebSite21VekByPage();
-
-        WebElement buttonAcceptCookie = driver.findElement(By.xpath(webSite21VekByPage.buttonAcceptCookieLocator));
-        buttonAcceptCookie.click();
-
-        WebElement buttonAccount = driver.findElement(By.xpath(webSite21VekByPage.buttonAccountLocator));
-        buttonAccount.click();
-
-        WebElement buttonLogin = driver.findElement(By.xpath(webSite21VekByPage.buttonLoginLocator));
-        buttonLogin.click();
+        WebSite21VekByPage webSite21VekByPage = new WebSite21VekByPage(driver);
+        webSite21VekByPage.clickButtonAcceptCookie();
+        webSite21VekByPage.clickButtonAccount();
+        webSite21VekByPage.clickButtonLogin();
         Thread.sleep(2000);
-
-        WebElement buttonLoginSubmit = driver.findElement(By.xpath(webSite21VekByPage.buttonLoginSubmitLocator));
-        buttonLoginSubmit.click();
-
-        WebElement errorMessageEmptyEmail = driver.findElement(By.xpath(webSite21VekByPage.errorMessageEmailLocator));
-        Assertions.assertEquals("Электронная почта не указана", errorMessageEmptyEmail.getText());
-
-        WebElement errorMessageEmptyPassword = driver.findElement(By.xpath(webSite21VekByPage.errorMessageEmptyPasswordLocator));
-        Assertions.assertEquals("Пароль не указан", errorMessageEmptyPassword.getText());
+        webSite21VekByPage.clickButtonLoginSubmit();
+        Assertions.assertEquals("Электронная почта не указана", webSite21VekByPage.getTextErrorMessageEmail());
+        Assertions.assertEquals("Пароль не указан", webSite21VekByPage.getTextErrorMessageEmptyPassword());
         Thread.sleep(2000);
         driver.quit();
     }
@@ -44,27 +29,14 @@ public class WebSite21VekByTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.21vek.by/");
-
-        WebSite21VekByPage webSite21VekByPage = new WebSite21VekByPage();
-
-        WebElement buttonAcceptCookie = driver.findElement(By.xpath(webSite21VekByPage.buttonAcceptCookieLocator));
-        buttonAcceptCookie.click();
-
-        WebElement buttonAccount = driver.findElement(By.xpath(webSite21VekByPage.buttonAccountLocator));
-        buttonAccount.click();
-
-        WebElement buttonLogin = driver.findElement(By.xpath(webSite21VekByPage.buttonLoginLocator));
-        buttonLogin.click();
+        WebSite21VekByPage webSite21VekByPage = new WebSite21VekByPage(driver);
+        webSite21VekByPage.clickButtonAcceptCookie();
+        webSite21VekByPage.clickButtonAccount();
+        webSite21VekByPage.clickButtonLogin();
         Thread.sleep(2000);
-
-        WebElement inputEmail = driver.findElement(By.xpath(webSite21VekByPage.inputEmailLocator));
-        inputEmail.sendKeys("test@test.com");
-
-        WebElement buttonLoginSubmit = driver.findElement(By.xpath(webSite21VekByPage.buttonLoginSubmitLocator));
-        buttonLoginSubmit.click();
-
-        WebElement errorMessageEmptyPassword = driver.findElement(By.xpath(webSite21VekByPage.errorMessageEmptyPasswordLocator));
-        Assertions.assertEquals("Пароль не указан", errorMessageEmptyPassword.getText());
+        webSite21VekByPage.sendKeysInputEmail("test@test.com");
+        webSite21VekByPage.clickButtonLoginSubmit();
+        Assertions.assertEquals("Пароль не указан", webSite21VekByPage.getTextErrorMessageEmptyPassword());
         Thread.sleep(2000);
         driver.quit();
     }
@@ -74,31 +46,17 @@ public class WebSite21VekByTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.21vek.by/");
-
-        WebSite21VekByPage webSite21VekByPage = new WebSite21VekByPage();
-
-        WebElement buttonAcceptCookie = driver.findElement(By.xpath(webSite21VekByPage.buttonAcceptCookieLocator));
-        buttonAcceptCookie.click();
-
-        WebElement buttonAccount = driver.findElement(By.xpath(webSite21VekByPage.buttonAccountLocator));
-        buttonAccount.click();
-
-        WebElement buttonLogin = driver.findElement(By.xpath(webSite21VekByPage.buttonLoginLocator));
-        buttonLogin.click();
+        WebSite21VekByPage webSite21VekByPage = new WebSite21VekByPage(driver);
+        webSite21VekByPage.clickButtonAcceptCookie();
+        webSite21VekByPage.clickButtonAccount();
+        webSite21VekByPage.clickButtonLogin();
         Thread.sleep(2000);
-
-        WebElement inputEmail = driver.findElement(By.xpath(webSite21VekByPage.inputEmailLocator));
-        inputEmail.sendKeys("test@t.com");
-
-        WebElement inputPassword = driver.findElement(By.xpath(webSite21VekByPage.inputPasswordLocator));
-        inputPassword.sendKeys("123456");
-
-        WebElement buttonLoginSubmit = driver.findElement(By.xpath(webSite21VekByPage.buttonLoginSubmitLocator));
-        buttonLoginSubmit.click();
+        webSite21VekByPage.sendKeysInputEmail("test@t.com");
+        webSite21VekByPage.sendKeysInputPassword("123456");
+        webSite21VekByPage.clickButtonLoginSubmit();
         Thread.sleep(2000);
-
-        WebElement errorMessageAccountDoesNotExist = driver.findElement(By.xpath(webSite21VekByPage.errorMessageEmailLocator));
-        Assertions.assertEquals("Нет такого аккаунта. \n" + "Зарегистрироваться?", errorMessageAccountDoesNotExist.getText());
+        webSite21VekByPage.getTextErrorMessageEmail();
+        Assertions.assertEquals("Нет такого аккаунта. \n" + "Зарегистрироваться?", webSite21VekByPage.getTextErrorMessageEmail());
         Thread.sleep(2000);
         driver.quit();
     }
